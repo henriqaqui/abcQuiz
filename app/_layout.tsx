af680a27f3440c24ce22748680561fa086f3010c
Henrique Rodrigues <henriqaqui@gmail.com>
2024-11-23 12:04:42 -0300

    Started project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from "expo-router";
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {useEffect} from 'react';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+export default function RoorLayout() {
+    const [loaded, error] = useFonts({
+        'UbuntuMono-Regular': require('@/assets/fonts/UbuntuMono-Regular.ttf'),
+        'FrederickatheGreat-Regular': require('@/assets/fonts/FrederickatheGreat-Regular.ttf'),
+      });
+    
+      useEffect(() => {
+        if (loaded || error) {
+          SplashScreen.hideAsync();
+        }
+      }, [loaded, error]);
+    
+      if (!loaded && !error) {
+        return null;
+      }
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    return (
+        <Stack screenOptions={{ headerShown: false}}>
+            <Stack.Screen name="index" />
+        </Stack>
+    );
 }
